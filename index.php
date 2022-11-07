426b83222793ff826ebc1b12b2232412ebed6e37
Rifqi Ammar Ramadhan <rifqiammar118@gmail.com>
2022-11-07 13:55:03 +0700

    Menambahkan List

--- index.php
+++ index.php
@@ -10,5 +10,11 @@
     <h1>Bismillah</h1>
 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus vitae tenetur velit quibusdam animi debitis, doloribus veniam modi inventore earum in eum alias cum perferendis, ratione repellat. Nihil, porro minima?</p>
+
+    <ul>
+        <li>
+            list
+        </li>
+    </ul>
 </body>
 </html>
